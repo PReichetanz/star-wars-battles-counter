9b67f685b87036ac2a8eca1b77609aed9ea152c7
PReichetanz <paul.reichetanz@gmx.de>
2022-09-20 22:39:03 +0200

    adapt DaySchema's battles array

--- api/_database/models/Day.js
+++ api/_database/models/Day.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
+const BattlesSchema = new mongoose.Schema({
+  side: String,
+  points: Number,
+});
+
 const DaySchema = new mongoose.Schema({
   date: {
     day: Number,
     month: Number,
     year: Number,
   },
-  battles: [
-    {
-      side: String,
-      points: Number,
-    },
-  ],
+  battles: { type: [BattlesSchema], default: undefined },
 });
 
 export default mongoose.model("User", DaySchema, "days");
