fefdcdf26ddc62115c2b817deb9118fc4724625c
PReichetanz <paul.reichetanz@gmx.de>
2022-09-20 22:39:16 +0200

    add simple post route

--- api/days.js
+++ api/days.js
@@ -8,6 +8,22 @@ const handler = async (request, response) => {
     if (request.method === "GET") {
       const days = await Day.find({});
       return response.json(days);
+    } else if (request.method === "POST") {
+      const day = new Day({
+        date: {
+          day: new Date().getDate(),
+          month: new Date().getMonth(),
+          year: new Date().getFullYear(),
+        },
+        battles: [
+          {
+            side: request.body.side,
+            points: request.body.points,
+          },
+        ],
+      });
+      const newDay = await day.save();
+      return response.json(newDay);
     }
   } catch (error) {
     return response.status(500).json({ message: error.message });
