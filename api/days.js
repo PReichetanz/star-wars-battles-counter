30c25113cfb7e52c5f720a55fea5130e4011a72e
PReichetanz <paul.reichetanz@gmx.de>
2022-09-21 21:53:07 +0200

    refactor: use .upsert() to shorten post route

--- api/days.js
+++ api/days.js
@@ -1,12 +1,6 @@
 import connectToMongodb from "./_database/connect-to-mongodb";
 import Day from "./_database/models/Day.js";
 
-const currentDate = {
-  day: new Date().getDate(),
-  month: new Date().getMonth(),
-  year: new Date().getFullYear(),
-};
-
 const handler = async (request, response) => {
   try {
     await connectToMongodb();
@@ -15,37 +9,22 @@ const handler = async (request, response) => {
       const days = await Day.find({});
       return response.json(days);
     } else if (request.method === "POST") {
-      const isExistingDay = await Day.exists({ date: currentDate });
-      console.log("isExistingDay", isExistingDay);
-
-      if (isExistingDay) {
-        const updatedDay = await Day.findOneAndUpdate(
-          { date: currentDate },
-          {
-            $push: {
-              battles: { side: request.body.side, points: request.body.points },
-            },
-          },
-          { new: true }
-        );
-        return response.json(updatedDay);
-      } else {
-        const day = new Day({
+      const updatedDay = await Day.findOneAndUpdate(
+        {
           date: {
             day: new Date().getDate(),
             month: new Date().getMonth(),
             year: new Date().getFullYear(),
           },
-          battles: [
-            {
-              side: request.body.side,
-              points: request.body.points,
-            },
-          ],
-        });
-        const newDay = await day.save();
-        return response.json(newDay);
-      }
+        },
+        {
+          $push: {
+            battles: { side: request.body.side, points: request.body.points },
+          },
+        },
+        { new: true, upsert: true }
+      );
+      return response.json(updatedDay);
     }
   } catch (error) {
     return response.status(500).json({ message: error.message });
