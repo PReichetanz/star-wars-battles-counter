1827d13bb53f6b760b4b154bfca0dbfa8eace87a
PReichetanz <paul.reichetanz@gmx.de>
2022-09-21 21:45:18 +0200

    add post route for existing day

--- api/days.js
+++ api/days.js
@@ -1,6 +1,12 @@
 import connectToMongodb from "./_database/connect-to-mongodb";
 import Day from "./_database/models/Day.js";
 
+const currentDate = {
+  day: new Date().getDate(),
+  month: new Date().getMonth(),
+  year: new Date().getFullYear(),
+};
+
 const handler = async (request, response) => {
   try {
     await connectToMongodb();
@@ -9,21 +15,37 @@ const handler = async (request, response) => {
       const days = await Day.find({});
       return response.json(days);
     } else if (request.method === "POST") {
-      const day = new Day({
-        date: {
-          day: new Date().getDate(),
-          month: new Date().getMonth(),
-          year: new Date().getFullYear(),
-        },
-        battles: [
+      const isExistingDay = await Day.exists({ date: currentDate });
+      console.log("isExistingDay", isExistingDay);
+
+      if (isExistingDay) {
+        const updatedDay = await Day.findOneAndUpdate(
+          { date: currentDate },
           {
-            side: request.body.side,
-            points: request.body.points,
+            $push: {
+              battles: { side: request.body.side, points: request.body.points },
+            },
           },
-        ],
-      });
-      const newDay = await day.save();
-      return response.json(newDay);
+          { new: true }
+        );
+        return response.json(updatedDay);
+      } else {
+        const day = new Day({
+          date: {
+            day: new Date().getDate(),
+            month: new Date().getMonth(),
+            year: new Date().getFullYear(),
+          },
+          battles: [
+            {
+              side: request.body.side,
+              points: request.body.points,
+            },
+          ],
+        });
+        const newDay = await day.save();
+        return response.json(newDay);
+      }
     }
   } catch (error) {
     return response.status(500).json({ message: error.message });
